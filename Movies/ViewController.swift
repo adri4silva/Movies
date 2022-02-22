314238e8ea00b10b0350b1f35420674f691cd3e9
Adrian Silva <adri4silva@gmail.com>
2022-02-22 20:02:44 +0100

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Adrian Silva on 22/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
